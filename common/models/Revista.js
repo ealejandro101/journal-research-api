06e552927dc5f7d032d126292cd8f053ed9a0fa0
Alejandro Escobar <ealejandro101@gmail.com>
2018-11-01 14:56:48 -0500

    Creación de método remoto busqueda

---
+++ common/models/Revista.js
@@ -0,0 +1,11 @@
+/**
+ * Retorna un listado de revistas que coinciden con la busqueda
+ * @param {string} q Cadena de caracter para buscar por diferentes campos
+ * @param {Function(Error, object)} callback
+ */
+
+Revista.busqueda = function(q, callback) {
+  var revistas = "hola";
+  // TODO
+  callback(null, revistas);
+};
