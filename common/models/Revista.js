bd7642a932e0609fad3412ed5abf46d41a4deee2
Cristian Andres Marin Durango <camd1996@gmail.com>
2019-03-13 19:41:32 -0500

    Avance end point filtros

--- common/models/Revista.js
+++ common/models/Revista.js
@@ -1,10 +1,8 @@
 module.exports = function(Revista) {
-  /**
-   * Retorna un listado de revistas que coinciden con la busqueda
-   * @param {string} q Cadena de caracter para buscar por diferentes campos
-   * @param {Function(Error, object)} callback
-   */
   let methods = {
+    /* Segun el SCOPE segun la documentacion de LoopBack v3, retorna el formato que debe de tener cada
+      JSON del resultado de la consulta, unicamente toma los includes, esto con el fin de validar que este atributo este
+      en cada JSON, si no esta es porque no cumple la condicion del filtro */
     returnFormat (scope){
       let format = {}
       if(scope.include === undefined || scope.include === null || !Array.isArray(scope.include)){
@@ -19,10 +17,14 @@ module.exports = function(Revista) {
       }
       return format
     },
+    /* Valida que sea valido el formato del JSON segun otro json, si es una arreglo y es vacio tambien se sabe que no cumple y por lo tanto retorna false */
     isFormatValid (format, object){
       let jsonObject = JSON.parse(JSON.stringify(object))
       for (const key in format) {
         if(jsonObject[key] === undefined || jsonObject[key] === null){
+          return false
+        }
+        if(Array.isArray(jsonObject[key]) && jsonObject[key].length === 0){
           return false
         }
         if(!methods.isFormatValid(format[key], jsonObject[key])){
@@ -33,6 +35,11 @@ module.exports = function(Revista) {
     }
   }
 
+  /**
+   * Retorna un listado de revistas que coinciden con la busqueda
+   * @param {string} q Cadena de caracter para buscar por diferentes campos
+   * @param {Function(Error, object)} callback
+   */
   Revista.busqueda = function(q, callback) {
     var filter = {
       "where":{
@@ -51,13 +58,14 @@ module.exports = function(Revista) {
     });
   };
 
+  /* Realiza un filtro como Loopback v3 con include, solo que convierte la consulta "LEFT JOIN" a "JOIN" */
   Revista.filtrar = function(filtro, callback) {
     let jsonFilter = JSON.parse(JSON.stringify(filtro))
     let jsonFormat = {}
     if (Array.isArray(jsonFilter.include)) {
       jsonFormat = methods.returnFormat(jsonFilter)
     }
-    console.log("Formato de respuesta (Revista.js/Revista.filtrar): "+ JSON.stringify(jsonFormat));
+    // console.log("Formato de respuesta (Revista.js/Revista.filtrar): "+ JSON.stringify(jsonFormat));
     Revista.find(jsonFilter, function(err, instance) {
       let response = []
       for (const iterator of instance) {
@@ -81,7 +89,13 @@ module.exports = function(Revista) {
         "arg": "filtro",
         "type": "Object",
         "required": true,
-        "description": `El campo debe de tener un JSON con los estándares de los filtros de la documentación de loopback v3, con las condiciones: 1) El atributo include debe de ser un arreglo de JSONs; 2) el atributo include debe de estar en la raiz del JSON o hijo de un atributo SCOPE 3) Cada JSON del array INCLUDE debe de tener un atributo "relation", este servicio lista las revistas que cumplen estrictamente con la condición haciendo enfasis en las condiciones de otros modelos. example: {"include": [{"relation": "[relation1]","scope": {"fields": ["[attribute1]" ],"where": {"[attribute1]": "[value1]"}}},{"relation": "[relation2]"}]}`
+        "description": `El campo debe de tener un JSON con los estándares de los filtros de la documentación de loopback v3,
+                        con las condiciones: 1) El atributo include debe de ser un arreglo de JSONs; 2) el atributo include 
+                        debe de estar en la raiz del JSON o hijo de un atributo SCOPE 3) Cada JSON del array INCLUDE debe de
+                         tener un atributo "relation", este servicio lista las revistas que cumplen estrictamente con la 
+                         condición haciendo enfasis en las condiciones de otros modelos 4) Si el filtro es por ID se debe 
+                         de hacer con un "OR", no se sabe porque, error de loopback. example: 
+                         {"include": [{"relation": "[relation1]","scope": {"fields": ["[attribute1]" ],"where": {"[attribute1]": "[value1]"}}},{"relation": "[relation2]"}]}`
       }],
       returns: {
         arg: 'revistas',
