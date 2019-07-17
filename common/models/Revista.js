23db4a05fc07cecefd371d353d453b273e5a7472
Cristian Andres Marin Durango <camd1996@gmail.com>
2019-07-17 18:42:46 -0500

    avance paginacion-abcdario

--- common/models/Revista.js
+++ common/models/Revista.js
@@ -59,7 +59,10 @@ module.exports = function(Revista) {
   };
 
   /* Realiza un filtro como Loopback v3 con include, solo que convierte la consulta "LEFT JOIN" a "JOIN" */
-  Revista.filtrar = function(filtro, callback) {
+  Revista.filtrar = function(body, callback) {
+    let filtro = body.filters
+    let extra = body.extra
+
     let sentences = {
       revista: {
         sentence: ''
@@ -82,11 +85,17 @@ module.exports = function(Revista) {
       rindexaciones: {
         sentence: 'revista.id = rindexaciones.revista_id'
       },
+      rubicacion: {
+        sentence: 'revista.id = rubicacion.id'
+      },
       estado: {
         sentence: 'revista.id = rubicacion.id AND ciudad.id = rubicacion.ciudad_id AND ciudad.state_id = estado.id',
         models: [
           'rubicacion', 'ciudad', 'estado'
         ]
+      },
+      palabrasclave: {
+        sentence: 'revista.id = palabrasclave.revista_id',
       }
     }
     let baseSQL = 'SELECT DISTINCT revista.*'
@@ -111,21 +120,25 @@ module.exports = function(Revista) {
           }
           where += sentences[iterator.model].sentence
         }
-        if (iterator.response && iterator.response.length > 0) {//Si el filtro es sensillo
+        //Si el filtro es sensillo
+        if (iterator.response && iterator.response.length > 0) {
           if (where.length != 0) {
             where += ' AND '
           }
           where += ' ( '
           for (const res of iterator.response) {
-            where += `${iterator.model}.${iterator.attribute} = ${res} OR  `
+            if (Array.isArray(iterator.attribute)) {
+              for (const attr of iterator.attribute) {
+                where += `${iterator.model}.${attr} = ${res} OR  `
+              }
+            }else{
+              where += `${iterator.model}.${iterator.attribute} = ${res} OR  `
+            }
           }
           where = where.substring(0, where.length - 5)
           where += ' ) '
         }
         if (iterator.customQuery && iterator.customQuery.length > 0) {//Si el filtro es personalizado
-          
-          
-          
           if (where.length != 0) {
             where += ' AND '
           }
@@ -138,7 +151,7 @@ module.exports = function(Revista) {
         }
       }
     }
-    let query = baseSQL + ' FROM ' + from + (where?' WHERE ' + where:'')
+    let query = baseSQL + ' FROM ' + from + (where?' WHERE ' + where:'') + (extra.order?' ORDER BY ' + extra.order:'') + (extra.limit?' LIMIT ' + extra.limit:'') + (extra.limit && extra.page?' OFFSET ' + (extra.limit * (extra.page - 1)):'')
     console.log(query);
     
     Revista.dataSource.connector.execute(query, [] , function (err, data) {
