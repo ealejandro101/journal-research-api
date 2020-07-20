d03445464b1ca254fe795f941f2abaee875dadce
Cristian Andres Marin Durango <camd1996@gmail.com>
2020-07-19 21:05:30 -0500

    role admin

--- server/boot/script.js
+++ server/boot/script.js
@@ -14,7 +14,7 @@ function (err, models) {
   // Now we have a list of models keyed by the model name
   // Find the first record from the inventory
   console.log(JSON.stringify(models.Convocatoria.definition));//imprime el modelo
-});
+});*/
 module.exports = function(app) {
   let Editor = app.models.Editor
   let Role = app.models.Role;
@@ -36,4 +36,3 @@ module.exports = function(app) {
     });
   })
 }
-*/
